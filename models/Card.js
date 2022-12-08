c390888226410ad6d8cae51626b26eaf17300d0e
Evgenii Buslaev <eugenebus144@gmail.com>
2022-12-08 10:51:37 +0500

    Added likes to Card schema

--- models/Card.js
+++ models/Card.js
@@ -4,6 +4,7 @@ const Card = new mongoose.Schema({
   author: { type: String, required: true },
   title: { type: String, required: true },
   text: { type: String, required: true },
+  likes: { type: Number, default: 0 },
   createdAt: {
     type: Date,
     default: Date.now,
