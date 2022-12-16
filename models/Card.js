547a02b9e525cba2a9cfe5dec4ed5119df1f6af4
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-16 14:13:47 +0500

    Edited card Schema, added comments array

--- models/Card.js
+++ models/Card.js
@@ -4,12 +4,13 @@ const Card = new mongoose.Schema({
   author: { type: String, required: true },
   title: { type: String, required: true },
   text: { type: String, required: true },
+  picture: { type: String },
   likes: { type: Number, default: 0 },
+  comments: { type: Array },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-  picture: { type: String },
 });
 
 export default mongoose.model("Card", Card);
